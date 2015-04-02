1f3b657cc964e3a65f5bc5b33cf944a4f15fd66c
Tatyana Letschenok <tatyana.letschenok@itechart-group.com>
2015-04-01 18:04:19 -0700

    add version increment to build script

--- MyApplication/deploy_master.sh
+++ MyApplication/deploy_master.sh
@@ -1,7 +1,25 @@
+incrementVersion()
+{
+echo 'increment build version'
+file="app/version.properties"
+versionKey="VERSION_CODE"
+
+while IFS="=" read -r key value; do
+case "$key" in
+$versionKey) versionCode="$value" ;
+esac
+done < "$file"
+echo $versionCode
+versionCode=$((versionCode+1))
+echo $versionCode
+echo $versionKey=$versionCode > "app/version.properties"
+
+}
+
 build(){
     ./gradlew assembleRelease
 }
 
 echo 'start building android'
-build
+incrementVersion
 echo 'stop building android'
