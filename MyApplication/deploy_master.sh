c48cd7a57f295a99852e335446dce359282157b0
Tatyana Letschenok <tatyana.letschenok@itechart-group.com>
2015-04-03 11:32:46 -0700

    update build version in repo

--- MyApplication/deploy_master.sh
+++ MyApplication/deploy_master.sh
@@ -16,7 +16,10 @@ echo $versionKey=$versionCode > "app/version.properties"
 
 }
 
+
+
 setBuildVersion(){
+CIRCLE_SHA1
     versionKey="VERSION_CODE"    
     touch "app/version.properties"
     echo $versionKey=$CIRCLE_BUILD_NUM > "app/version.properties"
@@ -27,8 +30,14 @@ echo 'upload debug build to Fabric'
     ./gradlew assembleDebug crashlyticsUploadDistributionDebug
 }
 
+commitNewVersion(){
+git commit -m 'update build version [ci skip]'
+git push
+}
+
 echo 'start building android'
-#incrementVersion
-setBuildVersion
+incrementVersion
+#setBuildVersion
 buildDebug
+commitNewVersion
 echo 'stop building android'
