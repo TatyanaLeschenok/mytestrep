2ba5955ca89e481d8513ac3238e32df7b0af07f7
Tatyana Letschenok <tatyana.letschenok@itechart-group.com>
2015-04-03 12:04:09 -0700

    add email

--- MyApplication/deploy_master.sh
+++ MyApplication/deploy_master.sh
@@ -33,7 +33,8 @@ echo 'upload debug build to Fabric'
 commitNewVersion(){
 echo $CIRCLE_USERNAME
 echo $CIRCLE_USEREMAIL
-git config --global user.name $CIRCLE_USERNAME
+git config user.name $CIRCLE_USERNAME
+git config user.email "circleci@abc.com"
 git commit -m 'update build version [ci skip]'
 git config --global push.default simple
 }
