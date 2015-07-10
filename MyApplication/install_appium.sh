9523b9c0e3dbaac1dd036ea2f918b419078b6de4
testuser <skydream@mail.ru>
2015-07-10 15:34:39 -0700

    update circle.yml file, add install, run scripts

--- MyApplication/install_appium.sh
+++ MyApplication/install_appium.sh
@@ -1,3 +1,3 @@
 echo "start appium installation"
-npm install -g appium  # get appium
+npm install -g appium
 npm install wd
--- MyApplication/run_appium.sh
+++ MyApplication/run_appium.sh
@@ -1,3 +1,1 @@
-appium -p 4723 -a 127.0.0.1:
-      background: true
-    sleep 5
+appium -p 4723 -a 127.0.0.1 &
