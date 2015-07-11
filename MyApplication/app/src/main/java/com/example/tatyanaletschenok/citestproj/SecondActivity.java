0653091c7eb9ff0d259a70c4f333a55f083c0e6a
testuser <skydream@mail.ru>
2015-07-10 17:12:01 -0700

    add app sample to test

--- MyApplication/app/src/main/java/com/example/tatyanaletschenok/citestproj/SecondActivity.java
+++ MyApplication/app/src/main/java/com/example/tatyanaletschenok/citestproj/SecondActivity.java
@@ -1,40 +1,23 @@
 package com.example.tatyanaletschenok.citestproj;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-
-import com.crashlytics.android.Crashlytics;
-import io.fabric.sdk.android.Fabric;
 
 
-public class MainActivity extends ActionBarActivity {
+public class SecondActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
-        setContentView(R.layout.activity_main);
-
-        Button button = (Button)findViewById(R.id.new_screen_button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_second);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_second, menu);
         return true;
     }
 
